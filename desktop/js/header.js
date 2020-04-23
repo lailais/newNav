81cdd9831f7bccd4ebc2f3c25c35359ff13e6a7c
Martina Lai <malai@xogrp.com>
2020-04-23 19:27:39 +0800

    change header in desktop to mobile view

--- desktop/js/header.js
+++ desktop/js/header.js
@@ -1,31 +1,31 @@
 const DOMContentLoadedCB = function() {
   const ProfileTrigger = document.getElementById('ProfileTrigger')
   const MobileNavTrigger = document.getElementById('MobileNavTrigger')
-  // const SiteNavigation = document.getElementById('SiteNavigation')
+  const SiteNavigation = document.getElementById('SiteNavigation')
+  const SocialLinks = document.querySelector('.tbHeader .SocialLinks')
   const Profile = document.querySelector('.tbHeader .Profile')
   const background = document.getElementById('background')
 
-  if (ProfileTrigger) {
-    ProfileTrigger.addEventListener('click', (e) => {
-      stopPropagation(e)
-      const isSiteNavigationOpen = MobileNavTrigger.classList.contains('open')
-      closeSiteNavigation()
-  
-      ProfileTrigger.classList.toggle("open")
-      Profile.classList.toggle("open")
-  
-      if(!isSiteNavigationOpen) {
-        background.classList.toggle("open")
-      }
-    })
-  }
+  ProfileTrigger.addEventListener('click', (e) => {
+    stopPropagation(e)
+    const isSiteNavigationOpen = MobileNavTrigger.classList.contains('open')
+    closeSiteNavigation()
+
+    ProfileTrigger.classList.toggle("open")
+    SocialLinks.classList.toggle("open")
+    Profile.classList.toggle("open")
+
+    if(!isSiteNavigationOpen) {
+      background.classList.toggle("open")
+    }
+  })
 
   MobileNavTrigger.addEventListener('click', (e) => {
     stopPropagation(e)
-    const isProfileOpen = ProfileTrigger && ProfileTrigger.classList.contains('open')
+    const isProfileOpen = ProfileTrigger.classList.contains('open')
     closeProfile()
     MobileNavTrigger.classList.toggle("open")
-    // SiteNavigation.classList.toggle("open")
+    SiteNavigation.classList.toggle("open")
 
     if(!isProfileOpen) {
       background.classList.toggle("open")
@@ -39,13 +39,14 @@ const DOMContentLoadedCB = function() {
   })
 
   closeProfile = () => {
-    ProfileTrigger && ProfileTrigger.classList.remove("open")
-    Profile && Profile.classList.remove("open")
+    ProfileTrigger.classList.remove("open")
+    SocialLinks.classList.remove("open")
+    Profile.classList.remove("open")
   }
 
   closeSiteNavigation = () => {
     MobileNavTrigger.classList.remove("open")
-    // SiteNavigation.classList.remove("open")
+    SiteNavigation.classList.remove("open")
   }
 
   closeBackground = () => {
